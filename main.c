b81671c3a8749eabf8ee495553708df707884e07
EzequielS <ezequiel.signorini@gmail.com>
2023-11-14 09:41:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -7,11 +7,12 @@
 #define t 25 // tamanio del mapa
 #define t_in 4
 #define PUNTAJES "./puntajes.txt"
-#define RESET_COLOR ""
-#define VERDE_T ""
-#define ROJO_T ""
-#define AMARILLO_T ""
-#define BLANCO_T ""
+#define RESET_COLOR "\x1b[0m"
+#define VERDE_T "\x1b[32m"
+#define ROJO_T "\x1b[31m"
+#define AMARILLO_T "\x1b[33m"
+#define BLANCO_T "\x1b[37m"
+#define VIOLETA_T "\x1b[35m"
 
 // estructura del cuerpo y cabeza
 struct cuerpo
@@ -38,6 +39,13 @@ struct tipo_fruta
 	struct tipo_fruta *siguiente;
 };
 
+struct death
+{
+	char sprite1;
+	char sprite2;
+	struct death *siguiente;
+};
+
 
 void imprimir(char espacio[t][t], int *puntaje, struct fruta *manzana, struct tipo_fruta **front, struct tipo_fruta **back);
 void generar(char espacio[t][t], struct fruta *manzana);
@@ -52,6 +60,9 @@ void push(struct node **head, char nuevo_nombre[20], int nuevo_puntaje);
 void pop(struct node **head);
 void crear_nodo(struct tipo_fruta **front, struct tipo_fruta **back, char caracter);
 void borrar_nodo(struct tipo_fruta **front, struct tipo_fruta **back);
+void pila_push(struct death **stackptr);
+void pila_pop(struct death **stackptr);
+
 
 int main(int argc, char *argv[])
 {
@@ -62,75 +73,114 @@ int main(int argc, char *argv[])
 	cursorInfo.bVisible = FALSE;
 	SetConsoleCursorInfo(hConsole, &cursorInfo);
 	
+	
 	struct cuerpo snake[((t - 2) * (t - 2)) - 1];
 	struct fruta manzana;
 	char letra = 'w', ult = 'w', espacio[t][t];
 	int muerto = 0, puntaje = 0, auxx2 = 0, auxy2 = 0, opcion = 0;
 	int auxx = 0, auxy = 0; // para modificar el tamanio de la snake
 	int tam = t_in;			// tamanio inicial de la matriz
+	int mask=0;
 	struct tipo_fruta *front = NULL;
 	struct tipo_fruta *back = NULL;
+	struct death *stackptr = NULL;
 	
-	for(int i=0;i<3;i++){
-		int random = rand() % 3;
-		switch(random){
-		case 0:
-			crear_nodo(&front,&back,'#');
-			break;
-		case 1:
-			crear_nodo(&front,&back,')');
-			break;
-		case 2:
-			crear_nodo(&front,&back,'%');
-			break;
-		}
-	}
-	
-	// genero la matriz del mapa con los bordes y la manzana
-	generar(espacio, &manzana);
-	// creo la vivorita
-	spawn(espacio, snake);
-	// imprimo menu de inicio
-	opcion = menu(&letra);
-	
-	if (opcion == 1)
-	{
-		letra = 'w';
-		ult = 'w';
-		// mientras el jugador no haya perdido puede jugar
-		while (muerto == 0)
-		{
-			ult=movimiento(&letra,&auxx,&auxy,&auxx2,&auxy2,snake,espacio,&manzana,&tam,&muerto,ult,&puntaje,&front,&back);
-			
-			// muestra el frame
-			imprimir(espacio, &puntaje, &manzana, &front, &back);
-		}
-		
-		printf(ROJO_T "\n\n\n			GAME OVER\n\n\n\n" RESET_COLOR);
-		
-		cursorInfo.bVisible = TRUE;
+	while(1){
+		letra = 'w', ult = 'w';
+		muerto = 0, puntaje = 0, auxx2 = 0, auxy2 = 0, opcion = 0;
+		auxx = 0, auxy = 0; // para modificar el tamanio de la snake
+		tam = t_in;			// tamanio inicial de la matriz
+		mask=0;
+		front = NULL;
+		back = NULL;
+		stackptr = NULL;
+		cursorInfo.bVisible = FALSE;
 		SetConsoleCursorInfo(hConsole, &cursorInfo);
-		
-		if(puntaje != 0){
-			guardar_puntaje(puntaje);
+			
+		for(int i=0;i<3;i++){
+			int random = rand() % 3;
+			switch(random){
+			case 0:
+				crear_nodo(&front,&back,'#');
+				break;
+			case 1:
+				crear_nodo(&front,&back,')');
+				break;
+			case 2:
+				crear_nodo(&front,&back,'%');
+				break;
+			}
 		}
+		
+		// genero la matriz del mapa con los bordes y la manzana
+		generar(espacio, &manzana);
+		// creo la vivorita
+		spawn(espacio, snake);
+		// imprimo menu de inicio
+		opcion = menu(&letra);
+		if (opcion == 1)
+		{
+			letra = 'w';
+			ult = 'w';
+			// mientras el jugador no haya perdido puede jugar
+			while (muerto == 0)
+			{
+				ult=movimiento(&letra,&auxx,&auxy,&auxx2,&auxy2,snake,espacio,&manzana,&tam,&muerto,ult,&puntaje,&front,&back);
+				
+				// muestra el frame
+				imprimir(espacio, &puntaje, &manzana, &front, &back);
+			}
+			
+			for(int i=0;i<tam*2;i++){
+				
+				if(mask==0){
+					pila_push(&stackptr);
+					espacio[snake[tam-(i/2)-1].x][snake[tam-(i/2)-1].y] = stackptr->sprite1;
+					imprimir(espacio, &puntaje, &manzana, &front, &back);
+				}
+				else{
+					espacio[snake[tam-(i/2)-1].x][snake[tam-(i/2)-1].y] = stackptr->sprite2;
+					imprimir(espacio, &puntaje, &manzana, &front, &back);
+				}
+				
+				mask=~mask;
+			}
+			for(int i=0;i<tam;i++){
+				pila_pop(&stackptr);
+			}
+			
+			printf(ROJO_T "\n\n\n			GAME OVER\n\n\n\n" RESET_COLOR);
+			
+			cursorInfo.bVisible = TRUE;
+			SetConsoleCursorInfo(hConsole, &cursorInfo);
+			
+			if(puntaje != 0){
+				guardar_puntaje(puntaje);
+			}
+			
+			//system("cls");
+			printf("\n->[E] REINTENTAR\n->[ANYKEY] VOLVER AL MENU");
+			while (kbhit() != 1)
+			{
+			}
+		}
+		else if (opcion == 2)
+		{
+			ver_puntajes();
+		}
+		else
+		{
+			exit(0);
+		}
+		
+		struct tipo_fruta *temp = front;
+		
+		do{
+			temp = front;
+			front = front->siguiente;
+			free(temp);
+		} while(front->siguiente!=NULL);
 	}
-	else if (opcion == 2)
-	{
-		ver_puntajes();
-	}
-	else
-	{
-		exit(0);
-	}
-	
-	struct tipo_fruta *temp = front;
-	
-	do{
-		temp = front;
-		front = front->siguiente;
-		free(temp);
-	} while(front->siguiente!=NULL);
 	
 	return 0;
 }
@@ -150,6 +200,14 @@ void imprimir(char espacio[t][t], int *puntaje, struct fruta *manzana, struct ti
 			{
 				printf(ROJO_T "%c " RESET_COLOR, espacio[i][j]);
 			}
+			else if (espacio[i][j] == ')')
+			{
+				printf(AMARILLO_T "%c " RESET_COLOR, espacio[i][j]);
+			}
+			else if (espacio[i][j] == '%')
+			{
+				printf(VIOLETA_T "%c " RESET_COLOR, espacio[i][j]);
+			}
 			else
 			{
 				printf(VERDE_T "%c " RESET_COLOR, espacio[i][j]);
@@ -158,12 +216,20 @@ void imprimir(char espacio[t][t], int *puntaje, struct fruta *manzana, struct ti
 		printf("\n");
 	}
 	
-	printf("\n PUNTAJE: %d \t\t\t\t", *puntaje); // imprime el puntaje actual
+	printf(AMARILLO_T "\n PUNTAJE: %d \t\t\t\t" RESET_COLOR, *puntaje); // imprime el puntaje actual
 	
 	struct tipo_fruta *temp = *front;
 	
 	do{
-		printf(" %c ", temp->tipo);
+		if(temp->tipo == '#'){
+			printf(ROJO_T " %c " RESET_COLOR, temp->tipo);
+		}
+		else if(temp->tipo == ')'){
+			printf(AMARILLO_T " %c " RESET_COLOR, temp->tipo);
+		}
+		else if(temp->tipo == '%'){
+			printf(VIOLETA_T " %c " RESET_COLOR, temp->tipo);
+		}
 		temp = temp->siguiente;
 	} while((*back)->siguiente != temp);
 
@@ -322,7 +388,7 @@ void guardar_puntaje(int puntaje_nuevo)
 		}else
 		{
 			// pide que ingrese su nombre para registrarlo
-			printf("  Nuevo puntaje alto!\n  Ingrese su nombre: ");
+			printf("  Nuevo puntaje alto!\n  Ingrese su nombre (4 caracteres): ");
 			scanf(" %s", nombre_nuevo);
 			printf("  Su puntaje fue: %d", puntaje_nuevo);
 			fprintf(fpuntaje, "%s %d\n", nombre_nuevo, puntaje_nuevo);
@@ -342,7 +408,7 @@ void guardar_puntaje(int puntaje_nuevo)
 			{
 				if(0 == puntaje_agregado)
 				{
-					printf("  Nuevo puntaje alto!\n  Ingrese su nombre: ");
+					printf("  Nuevo puntaje alto!\n  Ingrese su nombre (4 caracteres): ");
 					scanf(" %s", nombre_nuevo);
 					printf("  Su puntaje fue: %d", puntaje_nuevo);
 					push(&head, nombre_nuevo, puntaje_nuevo);
@@ -357,7 +423,7 @@ void guardar_puntaje(int puntaje_nuevo)
 		
 		if(puntaje_agregado == 0 && ranking < 10) //Si no esta llena la lista de puntajes
 		{
-			printf("  Nuevo puntaje alto!\n  Ingrese su nombre: ");
+			printf("  Nuevo puntaje alto!\n  Ingrese su nombre (4 caracteres): ");
 			scanf(" %s", nombre_nuevo);
 			printf("  Su puntaje fue: %d", puntaje_nuevo);
 			push(&head, nombre_nuevo, puntaje_nuevo);
@@ -389,15 +455,16 @@ void guardar_puntaje(int puntaje_nuevo)
 			
 			fclose(fpuntaje);
 		}
+		struct node *temp = head;
+		
+		do{
+			temp = head;
+			head = head->siguiente;
+			free(temp);
+		} while(head->siguiente!=NULL);
 	}
 	
-	struct node *temp = head;
-	
-	do{
-		temp = head;
-		head = head->siguiente;
-		free(temp);
-	} while(head->siguiente!=NULL);
+
 }
 
 void ver_puntajes()
@@ -415,41 +482,116 @@ void ver_puntajes()
 	else
 	{
 		// lee la informacion y cierra el archivo
-		while (!feof(fpuntaje))
+		int count=0;
+		int terminado=0;
+		
+		printf(VERDE_T "\n                                         ....:,,;;:;,;;;;:c.c::c:;,:...\n                               ..:,,,;;::cc::c:::c:c:;::;;::;:;;coc;:ll:;;,...\n                            .:cldxoc:;,:::..:.,:cccclcllllc:;::;.,:::.,:..::ll:;,.\n                           :k0oc:,...,,:...:" BLANCO_T "llx00000Okd." VERDE_T ":;;,:::.,:.,;:c;...,,..;ol;;:.\n                         .,ldc,:.:,:....:,;" BLANCO_T "lOKOl" AMARILLO_T ";" BLANCO_T "ldkNMXc" VERDE_T ".....:,:...::;ldd:..,:. .,..ol;.\n                       :col;...::....:;,:;,," BLANCO_T "oOkl"AMARILLO_T "::" BLANCO_T "lxkxl" VERDE_T ":.:,;llc;;,..:..l0:.,,::.....:::oc.\n   .ll,,,,,,,,,,,,:::.;0Kxoc:.,:ldxdl,...:;,...:;;;;;,,;:cdkKXX0xdc.:cdkc. .:,,:....::.:lo,\n   :d;                ;00xdc,,ldkkxo;.....,c;;cccc:::c.ooodx........:::::,,,,:,,;;;;;:::;.oc  ..\n   ,d:                 .:;;cc:c;:c:;::;;clxx:,:::::                                     ld;xc  cl\n   ;o.                    .;x0d:.........oKl                                            ;Kdcx. .oc\n   ;o.                       v            v                                             :Klck,  ,d;\n   ,o.                                                                                  cKcck:  ,xl.\n");        
+
+		while (terminado==0)
 		{
-			fscanf(fpuntaje, "%s %d\n", nombre, &puntaje);
-			//printf("%s: %d puntos \n", nombre, puntaje);
+			count++;
+			switch(count){
+			case 1:
+				printf("   ,o.\t\t\t");
+				fscanf(fpuntaje, "%s %d\n", nombre, &puntaje);
+				printf(RESET_COLOR "%s: %d puntos\t\t", nombre, puntaje);
+				if(!feof(fpuntaje)){
+					fscanf(fpuntaje, "%s %d\n", nombre, &puntaje);
+					printf("%s: %d puntos\t\t", nombre, puntaje);
+				}
+				else{
+					printf("\t\t\t\t");
+				}
+				printf(VERDE_T "cKcck:  ,xl.\n");
+				break;
+			case 2:
+				printf("   ,o.\t\t\t");
+				if(!feof(fpuntaje)){
+					fscanf(fpuntaje, "%s %d\n", nombre, &puntaje);
+					printf(RESET_COLOR "%s: %d puntos\t\t", nombre, puntaje);
+				}
+				else{
+					printf("\t\t\t\t");
+				}
+				if(!feof(fpuntaje)){
+					fscanf(fpuntaje, "%s %d\n", nombre, &puntaje);
+					printf("%s: %d puntos\t\t", nombre, puntaje);
+				}
+				else{
+					printf("\t\t\t\t");
+				}
+				printf(VERDE_T ":Kcxl   :0l\n");
+				break;
+			case 3:
+				printf("   :o:\t\t\t");
+				if(!feof(fpuntaje)){
+					fscanf(fpuntaje, "%s %d\n", nombre, &puntaje);
+					printf(RESET_COLOR "%s: %d puntos\t\t", nombre, puntaje);
+				}
+				else{
+					printf("\t\t\t\t");
+				}
+				if(!feof(fpuntaje)){
+					fscanf(fpuntaje, "%s %d\n", nombre, &puntaje);
+					printf("%s: %d puntos\t\t", nombre, puntaje);
+				}
+				else{
+					printf("\t\t\t\t");
+				}
+				printf(VERDE_T "c0:.   :0d.\n");
+				break;
+			case 4:
+				printf("   :o:\t\t\t");
+				if(!feof(fpuntaje)){
+					fscanf(fpuntaje, "%s %d\n", nombre, &puntaje);
+					printf(RESET_COLOR "%s: %d puntos\t\t", nombre, puntaje);
+				}
+				else{
+					printf("\t\t\t\t");
+				}
+				if(!feof(fpuntaje)){
+					fscanf(fpuntaje, "%s %d\n", nombre, &puntaje);
+					printf("%s: %d puntos\t\t", nombre, puntaje);
+				}
+				else{
+					printf("\t\t\t\t");
+				}
+				printf(VERDE_T "o0:  .;oxl.\n");
+				break;
+			case 5:
+				printf("   :d,\t\t\t");
+				if(!feof(fpuntaje)){
+					fscanf(fpuntaje, "%s %d\n", nombre, &puntaje);
+					printf(RESET_COLOR "%s: %d puntos\t\t", nombre, puntaje);
+				}
+				else{
+					printf("\t\t\t\t");
+				}
+				if(!feof(fpuntaje)){
+					fscanf(fpuntaje, "%s %d\n", nombre, &puntaje);
+					printf("%s: %d puntos\t\t", nombre, puntaje);
+					terminado=1;
+				}
+				else{
+					printf("\t\t\t\t");
+					terminado=1;
+				}
+				printf(VERDE_T "dk.,colc,.\n");
+				break;
+			}
 		}
 		
+		printf("   .o;                                                                                 .xxlxo;.\n   .d;                                                                                 .x0x;\n   .dc                                                                                 .xk:\n    ,:                                                                                 .dl.\n    ,:                                                                                 .dl.\n   .dc ,;;,,;;;;,,,,;::::;;;;;::;;;,,,,,:.....;;:;;;;....;;;...:;,,;cl:;;;;;;,,,,;;;;,,:kl:\n                                     .:odOKKNNWWWWNNN;:.,c:............:::::::,.      ;dlxk,\n                                   .oKWWWWXXXNNNN,::;::;c:.                           :dcokl.\n                                  ,OWWWMMMMMMMM;;;:.;ldd,                              ,occxko:\n                                .lO00KK00KXXX,.::,:.;kx.                                .ll;lK0:\n                                lNWWWWNXXXXX;.:,;;..lO;                                  :oc,l0Kc\n                               .lKXNNWWWWWWW.:;:.,:,lO;                                  :l:::kNO:\n                               .dKKKKKKKXXXX..:;;,:::kx.                               .;c;:.;OXO,\n                                :XMWWMMMMMMM,.:.::::::dd;.                           ,::;;:.;xNXl\n                                 :OK0KKKXXKKK..:.:,;,:;cloc:.                      ,cc,...;oKKx:.\n                                  ,kKKKXXXXKKK;..,..,:,.,c:ll:;:.               .cc;.,,,ck0ko;.\n                                   .cOXNNNNNNNN::..,c,.;c,.;c,:ol:::.........;:::,,;:dOOdo:.\n                                      .ck00000000KK,:..,;..,c,.:::;,;;;;:.:::::...d0kdxo;.\n                                        .;oOXWWWWWWNN;,:....:;:..:::;...;:,...lk0Kxcol:.\n                                           .::dOKKKKKXXKKX;:..:....;...cokOX0loxc:.\n                                               .;ldOXNWWWNXKK:;...:kdl0WX0dcccx.\n                                                    .:;;coxOKWWxc;:cccc,.." RESET_COLOR);
+		
 		fclose(fpuntaje);
+	}
+	while (kbhit() != 1)
+	{
 	}
 }
 
 int menu(char *letra)
 {
-	/*
-	int cambio=0;
-	
-	system("cls");
-	while (kbhit() != 1)
-	{
-	// imprime el menu
-	printf( VERDE_T "\n\n                         ....              ...::...                                                 \n                     ..::.......       ..:,::,,,,,::.                                               \n                ....::c;,::::,,;,::::,;;,,,:odxdlc:;::....                                          \n            ..::::::,:..::.....::..:;;,.,d00k0XKOkdd:.,;:.:::..                                     \n            :::;,,,,,;;;;:c;:......:;..:ONO;.c0XXK0Ol..,...::,:....                                 \n           .,cl:,;:.......::::,.......,dKXl..:OXXXX0l:..........:,,:.....                           \n           .,cc:;,.......,;;;,,.......,o0d...dKX0kl::..............:,,:::...                       \n              .::::,......,;,;..::.......:o:::xko;....::.::::........::..,;,:..                     \n                .,lc;;;,:.,;;;....... ............::::,;;;,:..:........:....,;:..                   \n                 .lc...:cll;;c;,,:............:;;:c::;::....::...............:,;:.                  \n                 .:l.   ;xl:,;.:,,,,,;;;;;;;:ll:,,,,:....,,:::..................;,.                 \n                  :l.   ,xc:;:.....;ccccllc::;........,;;::cll:::,..............:::.                \n                  .:,   ,o,:;...::lodool:;:.....,,,,,:oddxkkkddxkxol,............;;.                \n                  .:;   :;.;;.::odollc;......:,,,:....:ldxxxkkkkOOkOx;...........,;.                \n                 ..,.. .:.::;:oolll:,...:;,:::..  .    .,xOkkkkkkxxkkd;....:.....:,.                \n                 ...   ::.:llocllc,..,,,,...   .         :lxkkkxxxxxddd;.........;;.                \n                      .:.cdolccc;,,:::.                   .lOOOOOkdooodc:.......::,.                \n   ...              .:,,;clllc:,::..                       :dkxddddoodxo,.......;:.                 \n   ..:.         ...:;;,:. .....                            .:xxxxxxdoooc........:.                  \n    ..:.......:,;:;,:..                                    .:kxdddddddo,.......;;.                  \n      ..:,,;;,,::...                                       .lxdddooloo:.......;;.                   \n                                                          .;kxdoooooo:.......;,.                    \n                                                         .;okkxddool,......,;,..                    \n                                                       .,lxxxdoolc;.......,;..;. .                  \n                                                   ..::ldxxdoooc;........,,. .;;:..     .           \n                                              ..,;:coddoodocc;.........:,:. . :lc,:....  .          \n                                        ..:,;:lllllllllc;;;:.........:,:.     ..cc:,::.....         \n                                   ..:,;cloooolc::::;:............,,,:.         ..;ll;...:::..      \n                              ..:;;;:loollcc:;,:................;:,..              .cd;....:;,.     \n                  .........:;::cllc::cccc:,..........:.......:cc:,..::::::.:::::::::,,......;l,.    \n               ....:......:;,::,,,,;:;;,:............:::..::,;,:::::...........:...........::oc.    \n             ..,:............  .........  ...............  .............................,,;c:c;.    \n            .:,.................... .........   .. ................................:;;,:d.:;oc.     \n           .,cc;,;::,:,:..:::.........................................::........;:;;lo:,col:;.      \n           .cdo:lxxc:dxl,;llc;:::;:,c:;:.,::;;,,;::;,.:,,,::;,:,,,,:,:cc;:;cc:,,coc,cdl:cc,..       \n           .cdc:dxocokkl;ldo:cxdoc;oxoc,:doll:,cdool;::lcc;,cc;:llc;:lll:,:olc;::ol:loc,..          \n            .ccldxdcokxl:ddlcokdl::ddoc;lxdoo;;ddool,,lolc;:lc;cooc,;lllc,;oooc;ldl:,..             \n             ..:;:c:lxxolxkxodkxdcckkxocoxxxd:cxxdxo:cdddc:lol:looc:ldddo:cllc;::..                 \n                       :::,;:;:ccc:;ccc:;:lllc::llcc:;;cc:;,;:;,,,,::,,,::                          \n" RESET_COLOR );
-	if(cambio==0){
-	printf("\n\n			   	 PULSE" ROJO_T " CUALQUIER TECLA " RESET_COLOR "PARA COMENZAR");
-	printf("\n				      PULSE" AMARILLO_T " P " RESET_COLOR "PARA VER PUNTAJES");
-	
-}
-	
-	sleep(1);
-	// invierte el valor de cambio
-	cambio=~cambio;
-	// espera un cierto tiempo y refresca la pantalla
-	
-	system("cls");
-}
-	system("cls");
-	*/
 	int opcion = 1;
 	while (*letra != 'e')
 	{
@@ -479,6 +621,7 @@ int menu(char *letra)
 		while (kbhit() != 1)
 		{
 		}
+		
 		*letra = getch();
 		if (*letra == 'd')
 		{
@@ -563,7 +706,7 @@ void comer(struct fruta *manzana, struct cuerpo *snake, int *puntaje, int *tam, 
 			dif = 1;
 			manzana->x = 1 + rand() % (t - 2);
 			manzana->y = 1 + rand() % (t - 2);
-			for (int j = 0; j < (*tam - 1); j++)
+			for (int j = 0; j < (*tam)-1; j++)
 			{
 				if (manzana->x == snake[j].x && manzana->y == snake[j].y)
 				{
@@ -707,5 +850,26 @@ void borrar_nodo(struct tipo_fruta **front, struct tipo_fruta **back){
 	struct tipo_fruta *temp = *front;
 	
 	*front = (*front)->siguiente;
+	free(temp);
+}
+
+void pila_push(struct death **stackptr){
+	struct death *pila_nodo = NULL;
+	
+	pila_nodo = (struct death*) malloc(sizeof(struct death));
+	
+	pila_nodo->sprite1 = '*';
+	pila_nodo->sprite2 = ' ';
+	
+	pila_nodo->siguiente = *stackptr;
+	
+	*stackptr = pila_nodo;
+}
+	
+void pila_pop(struct death **stackptr){
+	struct death *temp = *stackptr;
+	
+	*stackptr = (*stackptr)->siguiente;
+	
 	free(temp);
 }
